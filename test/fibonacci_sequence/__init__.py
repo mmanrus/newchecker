78d9bba00891d28273951d5fc626ab5329b436f5
mmanrus <mmanrusiana@gmail.com>
2025-05-07 15:54:45 +0800

    Add fibonacci_test

---
+++ test/fibonacci_sequence/__init__.py
@@ -0,0 +1,20 @@
+import check50
+import re
+
+@check50.check()
+def exists():
+     check50.exists('fibonacci_sequence.py')
+     
+@check50.check(exists)
+def check_valueerror():
+    actual = check50.run("python3 fibonacci_sequence.py aa").stdout()  # No .stderr(), use .stdout()
+    if "ERROR: Please enter an integer" not in actual:
+        help = "ValueError: Be sure to convert argv[1] to int"
+        raise check50.Mismatch("ERROR: Please enter an integer", actual, help=help)
+
+@check50.check(exists)
+def check_outofbounds():
+    actual = check50.run("python3 fibonacci_sequence.py 1 2").stdout()  # No .stderr(), use .stdout()
+    if "USAGE: python fibonacci.py (n)" not in actual:
+        help = "Ensure the script only accepts a single argument: 'python fibonacci.py n>'"
+        raise check50.Mismatch("USAGE: python fibonacci.py (n)", actual, help=help)

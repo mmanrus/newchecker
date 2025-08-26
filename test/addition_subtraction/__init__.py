f72c3c092f47caf98b7c9f93bc3ff53d3e58d091
rai.emmanuel-rusiana <rai.rusianaemmanuel@gmail.com>
2025-08-26 13:39:10 +0800

    Fixing

--- test/addition_subtraction/__init__.py
+++ test/addition_subtraction/__init__.py
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-import check50
-
-@check50.check()
-def exists():
-    """Checks if addition_subtraction.c exists"""
-    check50.exists("addition_subtraction.c")
-
-@check50.check(exists)
-def compiles():
-    """Checks if addition_subtraction.c compiles successfully"""
-    check50.run("gcc -o addition_subtraction addition_subtraction.c").exit(0)
-    # or make addition_subtraction.c
-
-@check50.check(compiles)
-def checkFunctions():
-    """Checks if addition and subtraction work correctly for 12 and 34"""
-    result = check50.run("./addition_subtraction").stdin("12 34").stdout()
-
-    expected_output = "Sum: 46\nDifference: -22\n"
-
-    if expected_output not in result:
-        raise check50.Mismatch(expected_output, result, help="Expected output does not match.")
-
-@check50.check(compiles)
-def checkAgain():
-    """Checks if addition and subtraction work correctly for 20 and 25"""
-    result = check50.run("./addition_subtraction").stdin("20 25").stdout()
-
-    expected_output = "Sum: 45\nDifference: -5\n"
-
-    if expected_output not in result:
-        raise check50.Mismatch(expected_output, result, help="Expected output does not match.")
-=======
 import check50 # type: ignore
 
 @check50.check()
@@ -69,4 +35,3 @@ def checkAgain():
 def checkAgain():
     """Checks if Rejects \"A\""""
     check50.run("./addition_subtraction").stdin("A").exit(1, help="Input 'A' should cause the program to exit with an error.")
->>>>>>> b6afbcc (Testing what works)
